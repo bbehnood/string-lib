afd2f6761ef6a60386119f8ab845a9d0b3e29088
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-02-26 22:42:09 +0330

    Initial commit

---
+++ include/strlib.h
@@ -0,0 +1,17 @@
+#ifndef STRLIB_H
+#define STRLIB_H
+
+#include <stddef.h>
+
+typedef struct string
+{
+    char *data;
+    size_t length;
+    size_t capacity;
+} string_t;
+
+string_t string_new();
+string_t string_from(char *cstr);
+string_t string_free(string_t *str);
+
+#endif
